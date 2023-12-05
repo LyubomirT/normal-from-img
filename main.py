d3a84fbbd1ed451a9577486f27cfd39e3e72dc70
LyubomirT <terlyubomir@gmail.com>
2023-12-05 12:45:51 -0700

    Total Refactor and Reimagining

--- main.py
+++ main.py
@@ -2,60 +2,65 @@ from flask import Flask, render_template, request, flash
 import cv2
 import numpy as np
 import os
+import base64
 
 import uuid  # For generating unique filenames
 
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'uploads'
+import base64
+import cv2
+import numpy as np
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    normal_map_path = None
-    height_map_path = None
+import cv2
+import numpy as np
+import base64
 
-    if request.method == 'POST':
-        image = request.files['image']
-        if image.filename != '':
-            image_path = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
-            image.save(image_path)
-            img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-            normal_map = create_normal_map(img)
-            height_map = create_height_map(img)
-            
-            # Generate unique filenames for the normal_map and height_map
-            unique_id = str(uuid.uuid4().hex)
-            normal_map_path = os.path.join('static', 'uploads', f'normal_map_{unique_id}.png')
-            height_map_path = os.path.join('static', 'uploads', f'height_map_{unique_id}.png')
+def normal_map_from_base64(image_base64):
+  # decode the image base64 to a numpy array
+  image_data = base64.b64decode(image_base64)
+  image_array = np.frombuffer(image_data, dtype=np.uint8)
+  image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
 
-            # Clear the uploads folder
-            for file in os.listdir(os.path.join('static', 'uploads')):
-                os.remove(os.path.join('static', 'uploads', file))
-            
-            cv2.imwrite(normal_map_path, normal_map)
-            cv2.imwrite(height_map_path, height_map)
-            os.remove(image_path)  # Delete the uploaded image
-        else:
-            flash('Please upload an image')
-    
-    # remove the static folder from the path
-    normal_map_path = normal_map_path[7:] if normal_map_path else None
+  # convert the image to grayscale
+  gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # remove the static folder from the path
-    height_map_path = height_map_path[7:] if height_map_path else None
+  # apply a sobel filter to get the x and y gradients
+  sobel_x = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=3)
+  sobel_y = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=3)
 
-    return render_template('index.html', normal_map=normal_map_path, height_map=height_map_path)
+  # normalize the gradients to the range [0, 255]
+  sobel_x = cv2.normalize(sobel_x, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+  sobel_y = cv2.normalize(sobel_y, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
 
-def create_normal_map(img):
-    sobelx = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5)
-    sobely = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=5)
-    normal_map = np.dstack((sobelx, sobely, np.ones_like(img)))
-    normal_map = cv2.normalize(normal_map,None,alpha=0,beta=255,norm_type=cv2.NORM_MINMAX,dtype=cv2.CV_8U)
-    return normal_map
+  # create a normal map by stacking the gradients and a constant z value
+  normal_map = np.dstack((sobel_x, sobel_y, np.full_like(gray, 127)))
 
-def create_height_map(img):
-    height_map = cv2.Laplacian(img,cv2.CV_64F)
-    height_map = cv2.normalize(height_map,None,alpha=0,beta=255,norm_type=cv2.NORM_MINMAX,dtype=cv2.CV_8U)
-    return height_map
+  # encode the normal map to a base64 string
+  _, normal_map_data = cv2.imencode('.png', normal_map)
+  normal_map_base64 = base64.b64encode(normal_map_data).decode('utf-8')
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+  # return the normal map base64 string
+  return normal_map_base64
+
+
+
+
+img = input("Please enter the path to the image you want to convert to a normal map: ")
+
+# Read the image from the specified path
+image = cv2.imread(img)
+
+# Convert to base64
+image_bytes = cv2.imencode('.png', image)[1].tobytes()
+image_base64 = base64.b64encode(image_bytes).decode('utf-8')
+
+# Compute the normal map
+normal_map_base64 = normal_map_from_base64(image_base64)
+
+# Save the normal map to a file
+normal_map_bytes = base64.b64decode(normal_map_base64)
+normal_map_array = np.frombuffer(normal_map_bytes, dtype=np.uint8)
+normal_map = cv2.imdecode(normal_map_array, cv2.IMREAD_COLOR)
+cv2.imwrite('normal_map.png', normal_map)
+
+# Display the normal map in a window
+cv2.imshow('Normal Map', normal_map)
